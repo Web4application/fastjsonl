68bc4b83666a266a50dea22e631cc7133f7d238f
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-22 03:25:59 +0100

    Create Deploy.py

---
+++ Deploy.py
@@ -0,0 +1,13 @@
+fastjsonl
+ on 
+main
+ via 
+🐍
+ 3.13 
+✨
+(fastjsonl)
+❯
+ fastapi deploy
+✅ Deployment successful!
+
+🐔 Ready the chicken! Your app is ready at https://fastjsonl.fastapicloud.dev
